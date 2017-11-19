dc567accbface8ee7c2a48cb50e45c15cbd7f2b9
rayvie <32198894+rayvie@users.noreply.github.com>
2017-11-19 23:02:08 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,408 @@
+<?php
+include('db.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>HOTEL HOTEL</title>
+
+
+
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+
+<link href="css/font-awesome.css" rel="stylesheet"/>
+
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+
+<!--fonts-->
+<link href="//fonts.googleapis.com/css?family=Oswald:300,400,700" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Federo" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
+<!--//fonts-->
+</head>
+<body>
+<!-- header -->
+<div class="w3_navigation">
+	<div class="container">
+		<nav class="navbar navbar-default">
+			<div class="navbar-header navbar-left">
+				
+				<h1><a class="navbar-brand" href="index.php">HOTEL<span>HOTEL</span><p class="logo_w3l_agile_caption">Welcome in our Resort</p></a></h1>
+			</div>
+			<div class="navbar-right" id="navbar-1">
+				<nav class="menu ">
+					<ul class="nav navbar-nav menu__list">
+						<li class="menu__item"><a href="index.php" class="menu__link">Home</a></li>
+						<li class="menu__item"><a href="#about" class="menu__link scroll">About</a></li>
+						<li class="menu__item"><a href="#gallery" class="menu__link scroll">Rooms</a></li>
+					</ul>
+				</nav>
+			</div>
+		</nav>
+
+	</div>
+</div>
+<!-- //header -->
+		<!-- banner -->
+	<div id="home" class="w3ls-banner">
+		<!-- banner-text -->
+		<div class="slider">
+			<div class="callbacks_container">
+				<ul class="rslides callbacks callbacks1" id="slider4">
+					<li>
+						<div class="w3layouts-banner-top">
+
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4>HOTEL HOTEL</h4>
+									<h3>We know what you love</h3>
+										<p>Welcome to our hotels</p>
+									<div class="agileits_w3layouts_more menu__item">
+				<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">Learn More</a>
+			</div>
+								</div>	
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="w3layouts-banner-top w3layouts-banner-top1">
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4>HOTEL HOTEL</h4>
+									<h3>Stay with friends & families</h3>
+										<p>Come & enjoy precious moment with us</p>
+									<div class="agileits_w3layouts_more menu__item">
+				<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">Learn More</a>
+			</div>
+								</div>	
+							</div>
+						</div>
+					</li>
+					<li>
+						<div class="w3layouts-banner-top w3layouts-banner-top2">
+							<div class="container">
+								<div class="agileits-banner-info">
+								<h4>HOTEL HOTEL</h4>
+								<h3>want luxurious vacation?</h3>
+										<p>Get an accommodation today</p>
+									<div class="agileits_w3layouts_more menu__item">
+											<a href="#" class="menu__link" data-toggle="modal" data-target="#myModal">Learn More</a>
+										</div>
+								</div>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
+			<div class="clearfix"> </div>
+			<!--banner Slider starts Here-->
+		</div>
+		    <!--<div class="thim-click-to-bottom">
+				<a href="#about" class="scroll">
+					<i class="fa fa-long-arrow-down" aria-hidden="true"></i>
+				</a>
+			</div>-->
+	</div>	s
+	<!-- //banner --> 
+<!--//Header-->
+<!-- //Modal1 -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog">
+						<!-- Modal1 -->
+							<div class="modal-dialog">
+							<!-- Modal content-->
+								<div class="modal-content">
+									<div class="modal-header">
+										<button type="button" class="close" data-dismiss="modal">&times;</button>
+										<h4>HOTEL <span>HOTEL</span></h4>
+										<img src="images/1.jpg" alt=" " class="img-responsive">
+										<h5>We know what you love</h5>
+										<p>Providing guests unique and enchanting views from their rooms with its exceptional amenities, makes Star Hotel one of bests in its kind.Try our food menu, awesome services and friendly staff while you are here.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+<!-- //Modal1 -->
+<div id="availability-agileits">
+<div class="col-md-12 book-form-left-w3layouts">
+	<a href="admin/reservation.php "><h2><span>ROOM RESERVATION</span><br><br>CLICK HERE!</h2></a>
+</div>
+
+			<div class="clearfix"> </div>
+</div>
+    
+    <div class="banner-bottom">
+		<div class="container">	
+			<div class="agileits_banner_bottom">
+				<h3><span>Experience a good stay, enjoy fantastic offers</span> Find our friendly welcoming reception</h3>
+			</div>
+        </div>
+    </div>
+
+    <div class="about-wthree" id="about">
+		  <div class="container"> 
+				   <div class="ab-w3l-spa">
+                            <h3 class="title-w3-agileits title-black-wthree" style="text-decoration: underline; font-size: 18px;">About Our HOTEL HOTEL</h3> 
+						   <p class="about-para-w3ls">View the wonderful city from above while experiencing futuristic accomodaton. Be part of Hotel Hotel's booming present as you enjoy the amazing delights and comfort that Hotel Hotel gives you. Explore the landmarks of the breathtaking views, unparalleled service and endless possibilities at the iconic Hotel Hotel.</p>
+              </div>
+              
+					<div>
+              <a href="images/g4.jpg" class="swipebox"><img src="images/g4.jpg" class="img-responsive" alt="/" style="float:left; border:5px solid #021a40;">
+				</a> 
+              </div>
+              <div style="margin-left: 630px; font-size: 18px;">
+                  <h3>Be a baby on a cradle</h3><br>
+              <p>With high quality bed sheets and foam matress that will relieve all the stress on your body. With our very comforting pillows that will surely give you sweet dreams and casts away those nightmares. In Hotel Hotel you will Sleep like a baby. You'll like this place. You'll love to stay in Hote Hotel.</p>
+              </div>
+              
+              <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+              
+             <div>
+              <a href="images/g3.jpg" class="swipebox"><img src="images/g3.jpg" class="img-responsive" alt="/" style="float:right; border:5px solid #021a40; ">
+				</a> 
+              </div> 
+              <div style="margin-right: 630px; font-size: 18px;">
+                  <h3>Relax and Enjoy the view</h3><br>
+              <p>With our featured parts of the hotel specifically placed where our customer can enjoy, relax and please their eyes at the same time. Our Hotel Hotel is placed on the best spot on the city that is why it is called the mother of all Hotels. Enjoy our beautiful infinity pool with wonderful lights and flavored water.</p>
+              </div>
+              
+              <br><br><br><br><br><br><br><br><br><br><br><br><br> <br><br><br>
+              
+             <div>
+              <a href="images/g5.jpg" class="swipebox"><img src="images/g5.jpg" class="img-responsive" alt="/" style="float:left; border:5px solid #021a40;">
+				</a> 
+              </div> 
+              <div style="margin-left: 630px; font-size: 18px;">
+                  <h3>Enjoy nature with a TWIST!</h3><br>
+              <p> At first you will think that you are in a forest until you see those wonderful lights deep within. Enjoy nature with a twist is one of Hotel Hotel's mysterious feature. No one shares and explain what is inside Hotel Hotel's forest. Only you yourself can unravel the secret nature that lies within HOTEL HOTEL!</p>
+              </div>
+				
+        </div>
+        
+    </div>
+<h3 class="title-w3-agileits title-black-wthree" style="text-decoration: underline;">Our services</h3>
+    
+    <div class="container">
+    
+        <div>
+            <a href="images/s1.png" class="swipebox"><img src="images/s1.png" class="img-responsive" alt="/" style="float:left; border:5px solid #021a40;">
+				</a>
+            <div style="margin-left: 430px; font-size: 18px;">
+                <h3>24/7 security, front desk staffed & IT support</h3><br>
+              <p>The first thing you will see is the smiles of our staff when you approach our Hotel Hotel. An everlasting smile and very polite staff is will never get tired accomodating you. We are all equip with every skilled person in our hotel, from the security guard to the Owner itself. Wonderful staff is our biggest pride in Hotel Hotel.</p>
+            </div>
+        </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br>
+        <div>
+            <a href="images/s2.png" class="swipebox"><img src="images/s2.png" class="img-responsive" alt="/" style="float:right; border:5px solid #021a40;">
+				</a>
+            <div style="margin-right: 430px; font-size: 18px;">
+                <h3>Cleanliness is next to Godliness</h3><br>
+              <p>In Hotel Hotel cleanliness is a very strong religion. We are very pious when it comes to the hygenic portion of our hotel. We don't welcome any kind of dirt that lingers around our hotel. We keep our hotel clean to keep our prestige on its finest.</p>
+            </div>
+        </div>
+        <br><br><br><br><br><br><br><br><br><br><br><br><br>
+        <div>
+            <a href="images/s3.jpg" class="swipebox"><img src="images/s3.jpg" class="img-responsive" alt="/" style="float:left; border:5px solid #021a40;">
+				</a>
+            <div style="margin-left: 430px; font-size: 18px;">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscin</h3><br>
+              <p>Guests staying at the Hotel Hotel have access to a range of facilities that will help to ensure a comfortable and enjoyable stay.<br><br>
+
+• Daily complimentary buffet breakfast<br>
+
+• 24 hour Concierge<br>
+
+• Room service<br>
+
+• Laundry and dry cleaning service<br>
+
+• Luggage room<br>
+
+• Free Wireless internet access throughout the building<br>
+
+• Daily housekeeping service included in the room rate<br>
+
+• Butler service on request<br>
+
+• No minimum-stay requirement<br>
+
+• Doctor on call<br>
+
+            </div>
+        </div>
+        <br><br><br><br>
+        <div>
+            <a href="images/s4.png" class="swipebox"><img src="images/s4.png" class="img-responsive" alt="/" style="float:right; border:5px solid #021a40;">
+				</a>
+            <div style="margin-right: 430px; font-size: 18px;">
+                <h3>Just call our staffs and we'll be there</h3><br>
+              <p>Our staff are handpicked from millions of applicants. They are naturally respectful, modest and loyal. When you are enjoying your stay at our Hotel Hotel you will feel like royalty with hundreds of servants. As we said our Satff is our biggest pride in Hotel Hotel. They are not just staff, they are your helper, friend and family. We give a royalty quality service. </p>
+            </div>
+        </div>
+        
+        
+    </div>
+
+<br><br>
+
+<!-- Gallery -->
+<section class="portfolio-w3ls" id="gallery">
+		 <h3 class="title-w3-agileits title-black-wthree" style="text-decoration: underline;">Our Rooms</h3>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/img1.png" class="swipebox"><img src="images/img1.png" class="img-responsive" height="600" width="420" alt="/" style="border:1px solid #000000;">
+				</a>    
+				</div>
+    
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g2.jpg" class="swipebox"><img src="images/g2.jpg" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>    
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/img2.png" class="swipebox"><img src="images/img2.png" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>    
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g4.jpg" class="swipebox"><img src="images/g4.jpg" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>   
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/img3.png" class="swipebox"><img src="images/img3.png" class="img-responsive" alt="/" style="border:1px solid #000000;">
+					</a>
+                <br>    
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g6.jpg" class="swipebox"><img src="images/g6.jpg" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				   </a>
+                <br>    
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/img4.png" class="swipebox"><img src="images/img4.png" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				   </a>
+                <br>
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g6.jpg" class="swipebox"><img src="images/g8.jpg" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				   </a>
+                <br>    
+				</div>
+					<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g9.png" class="swipebox"><img src="images/g9.png" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>        
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g10.jpg" class="swipebox"><img src="images/g10.jpg" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>    
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g11.png" class="swipebox"><img src="images/g11.png" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>    
+				</div>
+				<div class="col-md-3 gallery-grid gallery1">
+					<a href="images/g12.png" class="swipebox"><img src="images/g12.png" class="img-responsive" alt="/" style="border:1px solid #000000;">
+				</a>
+                <br>    
+				</div>
+				<div class="clearfix"> </div>
+</section>
+<!-- //gallery -->
+<!--/footer -->
+    <div class="copy">
+		        <p>© 2017 HOTEL HOTEL . All Rights Reserved | Design by <a href="index.php">HOTELHOTEL</a> </p>
+		    </div>
+<!-- js -->
+<script type="text/javascript" src="js/jquery-2.1.4.min.js"></script>
+
+<!-- gallery popup -->
+<link rel="stylesheet" href="css/swipebox.css">
+				<script src="js/jquery.swipebox.min.js"></script> 
+					<script type="text/javascript">
+						jQuery(function($) {
+							$(".swipebox").swipebox();
+						});
+					</script>
+<!-- //gallery popup -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(function(event){		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+<!-- flexSlider -->
+				<script defer src="js/jquery.flexslider.js"></script>
+				<script type="text/javascript">
+				$(window).load(function(){
+				  $('.flexslider').flexslider({
+					animation: "slide",
+					start: function(slider){
+					  $('body').removeClass('loading');
+					}
+				  });
+				});
+			  </script>
+			<!-- //flexSlider -->
+<script src="js/responsiveslides.min.js"></script>
+			<script>
+						$(function () {
+						  
+						  $("#slider4").responsiveSlides({
+							auto: true,
+							pager:true,
+							nav:false,
+							speed: 500,
+							namespace: "callbacks",
+							before: function () {
+							  $('.events').append("<li>before event fired.</li>");
+							},
+							after: function () {
+							  $('.events').append("<li>after event fired.</li>");
+							}
+						  });
+					
+						});
+			</script>
+<!--tabs-->
+<script src="js/easy-responsive-tabs.js"></script>
+<script>
+$(document).ready(function () {
+$('#horizontalTab').easyResponsiveTabs({
+type: 'default', //Types: default, vertical, accordion           
+width: 'auto', //auto or any width like 600px
+fit: true,   // 100% fit in a container
+closed: 'accordion', // Start closed if in accordion view
+activate: function(event) { // Callback function if tab is switched
+var $tab = $(this);
+var $info = $('#tabInfo');
+var $name = $('span', $info);
+$name.text($tab.text());
+$info.show();
+}
+});
+$('#verticalTab').easyResponsiveTabs({
+type: 'vertical',
+width: 'auto',
+fit: true
+});
+});
+</script>
+	
+	<div class="arr-w3ls">
+	<a href="#home" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	</div>
+
+<script type="text/javascript" src="js/bootstrap-3.1.1.min.js"></script>
+</body>
+</html>
+
+
