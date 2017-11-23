7074626ea6fe2fd022afef2f9db235ec0bee8007
yarninel <32265036+yarninel@users.noreply.github.com>
2017-11-22 20:55:18 -0800

    Add files via upload

---
+++ Docu/payment.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2017 at 03:39 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ljlk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `id` int(11) DEFAULT NULL,
+  `title` varchar(5) DEFAULT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `lname` varchar(30) DEFAULT NULL,
+  `troom` varchar(30) DEFAULT NULL,
+  `tbed` varchar(30) DEFAULT NULL,
+  `nroom` int(11) DEFAULT NULL,
+  `cin` date DEFAULT NULL,
+  `cout` date DEFAULT NULL,
+  `ttot` double(8,2) DEFAULT NULL,
+  `fintot` double(8,2) DEFAULT NULL,
+  `mepr` double(8,2) DEFAULT NULL,
+  `meal` varchar(30) DEFAULT NULL,
+  `btot` double(8,2) DEFAULT NULL,
+  `noofdays` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
